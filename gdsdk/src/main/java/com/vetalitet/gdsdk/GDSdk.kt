95c438f1e8893d93a4e721df516b954519981e28
Vitalii Bilov <vbey@softserveinc.com>
2022-01-14 22:54:02 +0200

    -

--- gdsdk/src/main/java/com/vetalitet/gdsdk/GDSdk.kt
+++ gdsdk/src/main/java/com/vetalitet/gdsdk/GDSdk.kt
@@ -1,9 +1,19 @@
 package com.vetalitet.gdsdk
 
+import com.vetalitet.themes.Theme
+import com.vetalitet.themes.ThemeProvider
+
 class GDSdk {
 
     fun provideTheme(theme: GDTheme) {
-        
+        //ThemeProvider.setThemes(mapThemes(theme))
+    }
+
+    private fun mapThemes(theme: GDTheme): Theme {
+        return Theme.Builder()
+            .bgColor(theme.bgColor)
+            .textColor(theme.textColor)
+            .build()
     }
 
 }
