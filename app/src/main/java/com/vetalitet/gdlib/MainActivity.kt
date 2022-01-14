c9430345c8d32a9d28f0e3ceea07df9e954ce9ad
Vitalii Bilov <vbey@softserveinc.com>
2022-01-14 10:11:59 +0200

    fixed lib

--- app/src/main/java/com/vetalitet/gdlib/MainActivity.kt
+++ app/src/main/java/com/vetalitet/gdlib/MainActivity.kt
@@ -1,13 +1,16 @@
 package com.vetalitet.gdlib
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.vetalitet.gdsdk.GDSdkActivity
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        startActivity(Intent(this, GDSdkActivity::class.java))
     }
 
 }
