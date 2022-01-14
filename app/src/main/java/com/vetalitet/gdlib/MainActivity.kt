0d9d452bd30f159a9ad932a50e5445b5d112e11b
Vitalii Bilov <vbey@softserveinc.com>
2022-01-14 10:41:56 +0200

    1.2

--- app/src/main/java/com/vetalitet/gdlib/MainActivity.kt
+++ app/src/main/java/com/vetalitet/gdlib/MainActivity.kt
@@ -2,6 +2,7 @@ package com.vetalitet.gdlib
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import com.vetalitet.gdsdk.GDSdkActivity
 
@@ -10,7 +11,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        startActivity(Intent(this, GDSdkActivity::class.java))
+        findViewById<Button>(R.id.btn).setOnClickListener {
+            startActivity(Intent(this, GDSdkActivity::class.java))
+        }
     }
 
 }
