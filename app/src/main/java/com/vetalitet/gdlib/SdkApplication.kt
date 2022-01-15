e8d68089241a2efbf41ccc8e99397e16586af543
Vitalii Bilov <vbey@softserveinc.com>
2022-01-16 00:48:05 +0200

    -

--- app/src/main/java/com/vetalitet/gdlib/SdkApplication.kt
+++ app/src/main/java/com/vetalitet/gdlib/SdkApplication.kt
@@ -4,18 +4,17 @@ import android.app.Application
 import android.graphics.Color
 import com.vetalitet.gdsdk.GDSdk
 import com.vetalitet.gdsdk.GDTheme
-import kotlin.coroutines.coroutineContext
 
-class SdkApplication: Application() {
+class SdkApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
-        //GDSdk().provideTheme(createTheme())
+        GDSdk().provideTheme(createTheme())
     }
 
     private fun createTheme() = GDTheme.Builder()
-        .bgColor(Color.BLUE)
-        .textColor(Color.YELLOW)
+        .bgColor(Color.YELLOW)
+        .textColor(Color.BLUE)
         .build()
 
 }
